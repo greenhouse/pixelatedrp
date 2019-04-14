f9d644563497cea63e29f44c807f3d82323df10a
housesellout <housesellout@gmail.com>
2019-04-14 17:05:20 -0400

    :- synced kat’s changes 	“Fix map holes, add interior to Bahama Mamas building” 	“Added new garages across the map. Made text 'garage' for all blips so that they do not create their own list element in the map sidebar” 	“Translated ingame menus from French to English. File comments still in French” 	“add fingerpoint animation”

---
+++ server-data/resources/[esx]/esx_eden_garage/config.lua
@@ -0,0 +1,267 @@
+Config = {
+	DrawDistance = 100,
+	Price = 5000,
+	BlipInfos = {
+		Sprite = 290,
+		Color = 5 
+	}
+}
+
+Config.Garages = {
+	Centre = {	
+		Pos = {x=215.800, y=-810.057, z=29.727},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=229.700, y= -800.1149, z= 29.5722},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x=215.124, y=-791.377, z=29.646},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Paleto = {	
+		Pos = {x=105.359, y=6613.586, z=30.39},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=128.7822, y= 6622.9965, z= 29.78},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x=126.3572, y=6608.4150, z=29.85},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Morningwood = {	
+		Pos = {x=-1283.06, y=-214.37, z=41.45},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=-1274.77, y=-227.42, z=41.45},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x=-1277.15,y=-203.39,z=41.45},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	SandyShores = {	
+		Pos = {x=1695.16, y=3611.10, z=34.32},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=1701.83, y=3599.49, z=34.44},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x=1714.26,y=3597.23,z=34.31},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Chumash = {	
+		Pos = {x = -3140.323,y = 1124.463,z = 19.70},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = -3132.638,y = 1126.662,z = 19.66},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = -3136.902,y = 1102.685,z = 19.65},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Banham  = {	
+		Pos = {x = -2982.561,y = 327.506,z = 13.935},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = -2977.238,y = 337.777,z = 13.768},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = -2971.814,y = 355.331,z = 13.771},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Tattoo = {	
+		Pos = {x = 1319.09,y = -1663.01,z = 50.24},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = 1305.54,y = -1650.08,z = 50.58},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = 1311.54,y = -1663.77,z = 50.24},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Lake = {	
+		Pos = {x = -73.165504455566,y = 908.08734130859,z = 234.62},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = -72.099822998047,y = 902.85479736328,z = 234.63},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = -66.987632751465,y = 891.65881347656,z = 234.55},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Occaz = {	
+		Pos = {x = 472.00717163086,y = -1113.5531005859,z = 28.2},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = 465.24938964844,y = -1122.1062011719,z = 28.3},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = 480.43997192383,y = -1123.5560302734,z = 28.3},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Groove = {	
+		Pos = {x = -69.514305114746,y = -1822.9477539063,z = 25.941974639893 },
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = -59.95,y = -1842.81,z = 25.58},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = -60.4020652771,y = -1829.5288085938,z = 25.836933135986},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Venise = {	
+		Pos = {x = -1081.974609375,y = -1254.1881103516,z = 4.4244246482849 },
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = -1077.2590332031,y = -1239.3200683594,z = 4.161093711853},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = -1096.51,y = -1250.92,z = 4.17},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Tequila = {	
+		Pos = {x = -570.92272949219,y = 313.41049194336,z = 83.49},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = -569.47564697266,y = 323.53549194336,z = 83.47},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = -560.84375,y = 322.41586303711,z = 83.40},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	Clinton = {	
+		Pos = {x=364.55,y=297.51,z=102.49},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x = 378.00622558594,y = 288.13024902344,z = 102.16},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x = 364.76132202148,y = 285.18911743164,z = 102.37},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	MirrorPark = {	
+		Pos = {x=1033.9229736328, y=-767.10662841797, z=57.0},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=1040.6834716797, y=-778.18170166016, z=57.0},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x=1022.7816772461, y=-763.78955078125, z=57.0},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+	
+}
+
+
+
