ff1d0ad0ab57ec88017b768cc2add2246a0c489f
Kailin Cannon <kailin.cannon@gmail.com>
2019-04-16 10:05:47 -0600

    make mecanoActions waypoint slightly higher

--- server-data/resources/[esx]/esx_mecanojob/config.lua
+++ server-data/resources/[esx]/esx_mecanojob/config.lua
@@ -1,7 +1,7 @@
 Config                            = {}
 Config.DrawDistance               = 100.0
 Config.MaxInService               = -1
-Config.EnablePlayerManagement     = false
+Config.EnablePlayerManagement     = true
 Config.EnableSocietyOwnedVehicles = false
 Config.NPCSpawnDistance           = 500.0
 Config.NPCNextToDistance          = 25.0
@@ -19,7 +19,7 @@ Config.Vehicles = {
 Config.Zones = {
 
 	MecanoActions = {
-			Pos   = { x = -207.316, y = -1341.096, z = 33.89 },
+			Pos   = { x = -207.316, y = -1341.096, z = 34.00 },
 			Size  = { x = 1.5, y = 1.5, z = 1.0 },
 			Color = { r = 204, g = 204, b = 0 },
 			Type  = 1,
