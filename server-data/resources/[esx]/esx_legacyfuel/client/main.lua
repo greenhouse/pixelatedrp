c72ae3ce9b93d7f5ba355ae52f9182be97a8423a
Kailin Cannon <kailin.cannon@gmail.com>
2019-04-16 04:13:20 -0600

    hud changes

--- server-data/resources/[esx]/esx_legacyfuel/client/main.lua
+++ server-data/resources/[esx]/esx_legacyfuel/client/main.lua
@@ -511,7 +511,7 @@ function DisplayHud()
 		local speed   = GetEntitySpeed(vehicle)
 		local kmh     = round(speed * 3.6, 0)
 		local mph     = round(speed * 2.236936, 0)
-
+		local concatDisplay = mph .. "mp/h" .. kmh .. "km/h" .. fuel .. "fuel"
 		if fuel == 0 then
 			fuel = "0"
 		end
@@ -524,13 +524,14 @@ function DisplayHud()
 
 		x = 0.01135
 		y = 0.002
+
 		--DrawAdvancedText(x,y ,w,h,sc, text, r,g,b,a,font,jus)
 	--	DrawAdvancedText(0.2195 - x, 0.77 - y, 0.005, 0.0028, 0.6, fuel, 255, 255, 255, 255, 6, 1)
 
 	--	DrawAdvancedText(0.130 - x, 0.77 - y, 0.005, 0.0028, 0.6, mph, 255, 255, 255, 255, 6, 1)
 	--	DrawAdvancedText(0.174 - x, 0.77 - y, 0.005, 0.0028, 0.6, kmh, 255, 255, 255, 255, 6, 1)
 
-		DrawAdvancedText(0.248 - x, 0.7765 - y, 0.050, 0.0028, 0.4,.. mph .. "mp/h"   ..kmh..          "km/h"       ..fuel..       "Fuel", 255, 255, 255, 255, 6, 1)
+		DrawAdvancedText(0.248 - x, 0.7765 - y, 0.005, 0.0028, 0.4, concatDisplay, 255, 255, 255, 255, 6, 1)
 	end
 end
 
